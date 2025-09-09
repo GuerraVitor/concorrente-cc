001314b52019b32a186606957c2896dcd0d51e29
GuerraVitor <vitorhugoguerraguimaraes@gmail.com>
2025-09-09 15:53:50 -0300

    ajuste de comentarios

--- labs /lab5/entrega-lab5/ex4.c
+++ labs /lab5/entrega-lab5/ex4.c
@@ -8,16 +8,15 @@
 
 #define NTHREADS  4 // 2 threads A e 2 threads B
 
-/* Variaveis globais */
+// Variaveis globais 
 short int estado = 0; // Conta quantas threads A já passaram
 pthread_mutex_t mutex;
 pthread_cond_t cond;
 
-/* Thread A */
+// Thread A 
 void *A (void *t) {
   printf("Thread A: Comecei\n");
   
-  // Executa uma computação qualquer...
   printf("HELLO\n");
 
   // Sinaliza que terminou de executar 'HELLO'
@@ -29,11 +28,10 @@ void *A (void *t) {
   }
   pthread_mutex_unlock(&mutex);
 
-  // Executa outra computação qualquer...
   pthread_exit(NULL);
 }
 
-/* Thread B */
+// Thread B 
 void *B (void *t) {
   printf("Thread B: Comecei\n");
 
@@ -49,33 +47,32 @@ void *B (void *t) {
   // Executa 'BYEBYE'
   printf("BYEBYE\n");
   
-  // Executa outra computação qualquer...
   pthread_exit(NULL);
 }
 
 
-/* Funcao principal */
+// Funcao principal 
 int main(int argc, char *argv[]) {
   pthread_t threads[NTHREADS];
 
-  /* Inicializa o mutex e a variavel de condicao */
+  // Inicializa o mutex e a variavel de condicao
   pthread_mutex_init(&mutex, NULL);
   pthread_cond_init(&cond, NULL);
 
-  /* Cria as threads */
+  // Cria as threads 
   // As duas primeiras são A, as duas últimas são B. A ordem pode ser invertida.
   pthread_create(&threads[0], NULL, A, NULL);
   pthread_create(&threads[1], NULL, A, NULL);
   pthread_create(&threads[2], NULL, B, NULL);
   pthread_create(&threads[3], NULL, B, NULL);
 
-  /* Espera todas as threads completarem */
+  // Espera todas as threads completarem 
   for (int i = 0; i < NTHREADS; i++) {
     pthread_join(threads[i], NULL);
   }
   printf ("\nFIM\n");
 
-  /* Desaloca variaveis e termina */
+  // Desaloca variaveis e termina
   pthread_mutex_destroy(&mutex);
   pthread_cond_destroy(&cond);
   return 0;
